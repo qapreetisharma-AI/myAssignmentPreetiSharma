06f4ad1987afa56fb7d79e333382a32c0a61c977
qapreetisharma-AI <qapreetisharma@gmail.com>
2026-05-28 18:48:07 +0530

    Create primitiveDatatype.js

---
+++ assignmentWeek1/primitiveDatatype.js
@@ -0,0 +1,22 @@
+//declaring variables 
+let firstName = "Preeti"
+console.log(firstName)
+
+let companyName = "FIS"
+console.log(companyName)
+
+let mobileNumber = 9876543210
+console.log(mobileNumber)
+
+let isAutomation = true
+console.log(isAutomation)
+
+let hasPlaywright
+console.log(hasPlaywright) 
+
+//typeof
+console.log(typeof firstName) 
+console.log(typeof companyName)
+console.log(typeof mobileNumber)
+console.log(typeof isAutomation)
+console.log(typeof hasPlaywright) 
